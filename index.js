d5b4e44b9965f728d8bce590d04b3cade17f164b
Dany Pratmanto <danybms@gmail.com>
2022-06-05 06:06:28 +0700

    Tantangan Dome

--- index.js
+++ index.js
@@ -6,16 +6,24 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
-
+const sorter = (arrayNames, sortFunction)=>{ 
+  const numberingArray = [];
+  let set = sortFunction(arrayNames);
+  for (let i = 0; i<arrayNames.length; i++){
+   numberingArray.push(`${i+1}. ${set[i]}`);
+  }
+ return numberingArray;
+};
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
-
+const sortAscending = (names) =>{   
+  return names.sort();
+};
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+const sortDescending = (names) =>{
+  return names.sort().reverse();
+};
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
